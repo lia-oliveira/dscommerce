b47007eb360a4bc46d4d7493a6117f4542baf100
lia-oliveira <dev.gillian@gmail.com>
2025-05-03 22:58:26 -0300

    User and Order entity JPA annotations

--- dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/Order.java
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/Order.java
@@ -2,10 +2,60 @@ package com.oliveiralia.dscommerce.entities;
 
 import java.time.Instant;
 
+import com.oliveiralia.dscommerce.entities.enums.OrderStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
+	
 	private OrderStatus status;
 	
+	
+	private Order() {
+	}
+	
+	public Order(Long id, Instant moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 
 }
