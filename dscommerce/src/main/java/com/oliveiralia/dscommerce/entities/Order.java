b23518e64dd7d86970f1e01eea73434f1a688e21
lia-oliveira <dev.gillian@gmail.com>
2025-05-03 22:49:14 -0300

    Entities User Order and OrderStatus created

---
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/Order.java
@@ -0,0 +1,11 @@
+package com.oliveiralia.dscommerce.entities;
+
+import java.time.Instant;
+
+public class Order {
+	private Long id;
+	private Instant moment;
+	private OrderStatus status;
+	
+
+}
---
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.oliveiralia.dscommerce.entities.enums;
+
+public enum OrderStatus {
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+}
