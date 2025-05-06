64503ce8e61e32367d6352d465e9f4a559424e12
lia-oliveira <dev.gillian@gmail.com>
2025-05-05 21:13:26 -0300

    Get products and get orders

--- dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/Product.java
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/entities/Product.java
@@ -1,6 +1,7 @@
 package com.oliveiralia.dscommerce.entities;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -93,5 +94,13 @@ public class Product {
 
 	public Set<Category> getCategories() {
 		return categories;
-	}	
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Order> getOrders(){
+		return items.stream().map(x -> x.getOrder()).toList();
+	}
 }
