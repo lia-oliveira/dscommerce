4dc204eb5afb7de381004a4073fd9f4ee0eba100
lia-oliveira <dev.gillian@gmail.com>
2025-05-19 18:20:55 -0300

    Find All Pageable

--- dscommerce/src/main/java/com/oliveiralia/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/controllers/ProductController.java
@@ -1,8 +1,9 @@
 package com.oliveiralia.dscommerce.controllers;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,8 @@ public class ProductController {
 	private ProductService service;
 	
 	@GetMapping
-	public List<ProductDto> findAll() {
-		return service.findAll();
+	public Page<ProductDto> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 	
 	@GetMapping(value = "/{id}")
--- dscommerce/src/main/java/com/oliveiralia/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/oliveiralia/dscommerce/services/ProductService.java
@@ -1,10 +1,11 @@
 package com.oliveiralia.dscommerce.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,11 +21,9 @@ public class ProductService {
 	private ProductRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ProductDto> findAll(){
-		List<Product> result = repository.findAll();
-		return result.stream()
-				.map(ProductMapper::fromEntity)
-				.collect(Collectors.toList());
+	public Page<ProductDto> findAll(Pageable pageable){
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(ProductMapper::fromEntity);
 	}
 	
 	@Transactional(readOnly = true)
